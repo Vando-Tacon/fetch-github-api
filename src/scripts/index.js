7ec4d3e0dc833dce0e1e9698d616718d128671e2
Vando Tacon <vandotacon2@gmail.com>
2024-08-14 20:02:51 -0300

    primeiro commit

---
+++ src/scripts/index.js
@@ -0,0 +1,51 @@
+import { getUser } from './services/users.js'
+import { getRepositories } from './services/repositorioes.js'
+import { user } from './objects/user.js'
+import { screen } from './objects/screen.js'
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+  if(validadeEmptyInput(userName)) return
+    getUserData(userName)
+})
+
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed) {
+        if(validadeEmptyInput(userName)) return
+        getUserData(userName)
+    }
+})
+
+function validadeEmptyInput (userName){   
+    if(userName.length === 0){
+   alert('Preencha o campo com o nome de usuário do GitHub')
+   return true
+}
+}
+
+async function getUserData(userName) {
+
+  const userResponse  = await getUser(userName)
+
+  if(userResponse.message === "Not Found"){
+    screen.renderNotFound()
+    return
+  }
+
+  const repositoriesResponse  = await getRepositories(userName)
+  
+  user.setInfo(userResponse)
+  user.setRepositories(repositoriesResponse)
+  
+  screen.renderUser(user)
+    
+    
+}
+
+
+
